35b709f0e701f83e24a5f399b51f22655eeec1e5
Soniya Pawar <soniya000pawar@gmail.com>
2026-05-14 13:14:56 +0530

    Bubble sort

--- sort.cpp
+++ sort.cpp
@@ -1,85 +1,112 @@
 #include <iostream>
 #include <vector>
-#include <thread>
-#include <algorithm>
-#include <ctime>
+#include <omp.h>
+
 using namespace std;
 
-// Bubble Sort
-void bubble_sort(vector<int>& a) {
-    int n = a.size();
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (a[j] > a[j + 1]) {
-                swap(a[j], a[j + 1]);
+// Sequential Bubble Sort
+void sequential_bubble_sort(vector<int>& arr)
+{
+    int n = arr.size();
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
             }
         }
     }
 }
 
-// Parallel Sort
-void parallel_sort(vector<int>& arr) {
+// Parallel Bubble Sort using Odd-Even Transposition
+void parallel_bubble_sort(vector<int>& arr)
+{
     int n = arr.size();
-    int mid = n / 2;
 
-    vector<int> left(arr.begin(), arr.begin() + mid);
-    vector<int> right(arr.begin() + mid, arr.end());
+    for (int i = 0; i < n; i++)
+    {
+        // Even phase
+#pragma omp parallel for
+        for (int j = 0; j < n - 1; j += 2)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
 
-    thread t1(bubble_sort, ref(left));
-    thread t2(bubble_sort, ref(right));
-
-    t1.join();
-    t2.join();
-
-    // Merge manually (instead of sort for better compatibility)
-    int i = 0, j = 0, k = 0;
-    vector<int> result(n);
-
-    while (i < left.size() && j < right.size()) {
-        if (left[i] < right[j])
-            result[k++] = left[i++];
-        else
-            result[k++] = right[j++];
+        // Odd phase
+#pragma omp parallel for
+        for (int j = 1; j < n - 1; j += 2)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
     }
-
-    while (i < left.size()) result[k++] = left[i++];
-    while (j < right.size()) result[k++] = right[j++];
-
-    arr = result;
 }
 
-int main() {
+// Print Array
+void printArray(const vector<int>& arr)
+{
+    for (int x : arr)
+        cout << x << " ";
+
+    cout << endl;
+}
+
+int main()
+{
     int n;
+
     cout << "Enter number of elements: ";
     cin >> n;
 
     vector<int> arr(n);
+
     cout << "Enter elements: ";
-    for (int i = 0; i < n; i++) cin >> arr[i];
 
-    cout << "Original: ";
-    for (int x : arr) cout << x << " ";
-    cout << endl;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
 
-    // Sequential
-    vector<int> a1 = arr;
-    clock_t start = clock();
-    bubble_sort(a1);
-    clock_t end = clock();
+    // Create copies for both sorting methods
+    vector<int> seqArr = arr;
+    vector<int> parArr = arr;
 
-    cout << "Sequential: ";
-    for (int x : a1) cout << x << " ";
-    cout << "\nTime: " << (double)(end - start) / CLOCKS_PER_SEC << " sec\n";
+    // Sequential Bubble Sort Timing
+    double start_seq = omp_get_wtime();
 
-    // Parallel
-    vector<int> a2 = arr;
-    start = clock();
-    parallel_sort(a2);
-    end = clock();
+    sequential_bubble_sort(seqArr);
 
-    cout << "Parallel: ";
-    for (int x : a2) cout << x << " ";
-    cout << "\nTime: " << (double)(end - start) / CLOCKS_PER_SEC << " sec\n";
+    double end_seq = omp_get_wtime();
+
+    // Parallel Bubble Sort Timing
+    double start_par = omp_get_wtime();
+
+    parallel_bubble_sort(parArr);
+
+    double end_par = omp_get_wtime();
+
+    // Output Sequential Result
+    cout << "\nSequential Bubble Sort Result:\n";
+    printArray(seqArr);
+
+    // Output Parallel Result
+    cout << "\nParallel Bubble Sort Result:\n";
+    printArray(parArr);
+
+    // Print Execution Times
+    cout << "\nExecution Time:\n";
+
+    cout << "Sequential Bubble Sort Time: "
+         << (end_seq - start_seq) << " seconds\n";
+
+    cout << "Parallel Bubble Sort Time: "
+         << (end_par - start_par) << " seconds\n";
 
     return 0;
 }
